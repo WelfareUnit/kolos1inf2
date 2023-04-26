206854e8c23d77591083bc1dfd014b42d06bd60b
Jan Palczewski <palczewskijan13@gmail.com>
2023-04-26 14:15:19 +0200

    poprawki

--- main.cpp
+++ main.cpp
@@ -11,8 +11,8 @@
 //mamy dane, wprowadŸmy je - talbice bêd¹ 5cioelementowe statyczne na pocz¹tku, bo tak¹ mamy tabelkê
 int N = 5;
 /* //dla danych stacjonarnie pobranych
-double danex[5] = { -1.0,0.0,1.0,2.0,3.0 }; //double, ¿eby siê dobrze mno¿y³o
-double daney[5] = { -2.5,-1.5,-1,0.25,0.75 }; //liczby z kropk¹
+double danex[5] = {-1.0,0.0,1.0,2.0,3.0}; //double, ¿eby siê dobrze mno¿y³o
+double daney[5] = {-2.5,-1.5,-1,0.25,0.75}; //liczby z kropk¹
 */
 //dla danych z pliku
 double* danex;
@@ -39,7 +39,7 @@ void danezpliku()
 
 double funkcjazinterpolowana(double x) //do 3.
 {
-    return lagrange(danex, daney, 5, x);
+    return lagrange(danex, daney, N, x);
 }
 int main()
 {
@@ -52,36 +52,37 @@ int main()
    
     //sprawdzamy, ile bêdzie potrzebne kroków
     //pobranie danych z pliku
-    double ilekroków =(int)(danex[4] - danex[0])/h; //ofc elementy tablicy liczy siê od 0; dzielimy szerokoœæ dziedziny na krok i mamy ile kroków musimy przejœc
+    int ilekroków =(int)(danex[4] - danex[0])/h+1; //ofc elementy tablicy liczy siê od 0; dzielimy szerokoœæ dziedziny na krok i mamy ile kroków musimy przejœc
     double* interpolowanex = (double *)malloc(ilekroków*sizeof(double));
     double* interpolowaney = (double *)malloc(ilekroków*sizeof(double));
     //tu mo¿na na kilka sposobów, zobaczymy
-    int pom = 0;
-    for (double i = danex[0]; i <= danex[N]; i += h) //zaczynamy od pierwszego elementu z tablicy i lecimy do ostatniego co krok interpolowania
+    double pom = danex[0];
+    for (int i = 0; i < ilekroków; i ++) //zaczynamy od pierwszego elementu z tablicy i lecimy do ostatniego co krok interpolowania
     {
-        interpolowanex[pom] = i;
-        interpolowaney[pom] = lagrange(danex, daney, 5, i); //interpoluje wartoœæ y(i) na podstawie wartoœci wprowadzeonej na pocz¹tku    //   printf("%.5f\n", interpolowaney[pom]);
-        pom++;
+        interpolowanex[i] = pom;
+        interpolowaney[i] = lagrange(danex, daney, 5, pom); //interpoluje wartoœæ y(i) na podstawie wartoœci wprowadzeonej na pocz¹tku    //   printf("%.5f\n", interpolowaney[pom]);
+        pom+=h;
     }
     //2. zapisaæ do pliku, dwie kolumny
     FILE* f;
     f = fopen("interpol.txt", "w");
     fprintf(f, "x\ty\n"); //nag³ówek pliku
 
-    for (int i = 0; i <= ilekroków;i++)
+    for (int i = 0; i < ilekroków;i++)
     {
         fprintf(f,"%.5f\t%.5f\n", interpolowanex[i], interpolowaney[i]); //drukowanie do pliku po tabulatorku. .5f to 5 miejsc po przecinku
     }
     fclose(f);
     //3. Wyznaczyæ wartoœæ ca³ki oznaczonej z przedzia³u od O do 3 dla funkcji wygenerowanej w punkcie 2 (z krokiem h = O.025), wynik dzia³ania wyœwietl na ekranie
-    //double wartcalki = trapez(danex[0], danex[4], funkcjazinterpolowana, ilekroków);
+    double wartcalki = trapez(danex[0], danex[N-1], funkcjazinterpolowana, ilekroków);
+    printf("Wartoœæ ca³ki to %.5f\n", wartcalki);
     //4. Wyznaczyæ miejsce zerowe funkcji przybli¿onej danymi z dok³adnoœci¹ do 10 ^- 4, iloœæ iteracji wyœwietl na ekranie(4 pkt)
     int iter = 0;
     double epsilon = 0.0001;
     double mz = 0; //na miejsce zerowe
      //   
     mz = bisec(danex[0], danex[4], funkcjazinterpolowana, epsilon, &iter); //wartoœci funkcji na poczatku sa -, potem + _ musi byc miejsce zerowe pomiedzy. skorzystamy z bisekcji
-    printf("\nMiejscezerowe = %.4f, liczba iteracji bisekcji = %d", mz, iter);
+    printf("\nMiejsce zerowe = %.4f, liczba iteracji bisekcji = %d", mz, iter);
 
     //co do wykresu - wêz³y interpolacji np. mozna x-ami zaznaczyæ, wykres z interpolowanego ma³ymi kropkami
     free(interpolowanex);
