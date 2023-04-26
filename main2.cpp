fa8bf7d5173aac3f6741392e87314ebd8f5ee17d
Jan Palczewski <palczewskijan13@gmail.com>
2023-04-26 16:50:46 +0200

    opisy

--- main2.cpp
+++ main2.cpp
@@ -7,12 +7,12 @@
 #include "nonlin.h"
 double k = 1;
 
-double funkcjawew(double x)
+double funkcjawew(double x) //funkcja podca³kowa
 {
 	return cos(k * 3.14159265359* x );
 }
 
-double funkcjaodalfa(double alfa)
+double funkcjaodalfa(double alfa) //funkcja liczaca calke
 {
 	return trapez(0, alfa, funkcjawew, 2000); //2000 z dupy wziete zeby bylo dokladnie
 }
@@ -22,11 +22,14 @@ int main()
 	int alfa = 1;
 	int N = 0;
 	int epsilon = 0.0001;
-	while ((int)N > 200 || (int)N < 2)    //¿eby na pewno dobrze wpisali
+	do   //¿eby na pewno dobrze wpisali
 	{
 		printf("Podaj liczbe calkowita N, z przedzialu (1,200>: \n");
 		scanf("%d", &N);
-	} 
+		system("CLS"); //czyszczenie ekranu
+		printf("Niepoprawne dane\n");
+		N = -1;
+	} while ((int)N > 200 || (int)N < 2);
 	int iter = 0;
 	double miejsce_zerowe = 0;
 	for (int i = 1; i <= N; i++)
